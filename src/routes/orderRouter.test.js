d680a5725157443f724961e74e69395f03ceb251
Jonah Larsen <Jonah.Larsen@gmail.com>
2026-02-04 20:07:36 -0700

    getMenu

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -3,16 +3,45 @@ const {createAdmin, expectValidJwt, randomName} = require("./testUtils.js");
 const request = require('supertest');
 const app = require('../service');
 
+let adminUser;
+let adminUserAuthToken;
+let adminUserId;
 
-test('getMenu', async () => {
-    const getMenuRes = await request(app).get('/api/order/menu');
-    expect(getMenuRes.status).toBe(200);
-    expect(getMenuRes.body[0]).toMatchObject(
-        { 
-            id: 1, title: 'Veggie', 
-            image: 'pizza1.png', 
-            price: 0.0038, 
-            description: 'A garden of delight' 
+beforeAll(async () => {
+    adminUser = await createAdmin();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser);
+    expect(adminLoginRes.status).toBe(200);
+    expectValidJwt(adminLoginRes.body.token);
+    adminUserAuthToken = adminLoginRes.body.token;
+    adminUserId = adminLoginRes.body.user.id;
+})
+
+async function createMenuItem() {
+    const testMenuItemObject = {
+            'title': "testMenuItem",
+            'description': 'a pizza full of yummy test data',
+            'image': 'fakepizzaimage.png',
+            'price': 1.55
+        }
+    const createMenuItemRes = await request(app).put('/api/order/menu').set(
+        {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${adminUserAuthToken}`
         }
     )
+    .send(testMenuItemObject)
+    expect(createMenuItemRes.status).toBe(200);
+    expect(createMenuItemRes.body.at(-1)).toMatchObject(testMenuItemObject);
+
+    return {
+        createMenuItemRes: createMenuItemRes,
+        testMenuItemObject: testMenuItemObject
+    }
+}
+
+test('getMenu', async () => {
+    const {createMenuItemRes, testMenuItemObject} = await createMenuItem();
+    const getMenuRes = await request(app).get('/api/order/menu');
+    expect(getMenuRes.status).toBe(200);
+    expect(getMenuRes.body.at(-1)).toMatchObject(testMenuItemObject);
 })
