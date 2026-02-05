a7a6520f77711898117077b8022b14309b3c85a2
Jonah Larsen <Jonah.Larsen@gmail.com>
2026-02-04 21:06:33 -0700

    updateUser

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -9,11 +9,13 @@ let adminUserAuthToken;
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2,12) + '@test.com';
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
+    testUserId = registerRes.body.user.id;
     expectValidJwt(testUserAuthToken);
 
     adminUser = await createAdmin();
@@ -31,6 +33,23 @@ test('getUser', async () => {
 
     expect(getUserRes.status).toBe(200);
     expect(getUserRes.body.name).toBe(testUser.name);
+});
+
+
+test('updateUser', async () => {
+    const updateUserRes = await request(app).put(`/api/user/${testUserId}`)
+    .set({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${testUserAuthToken}`
+    })
+    .send({
+        name: "newTestUserName",
+        email: "newTestUserEmail@test.com",
+        password: "newsecretpassword"
+    });
+    expect(updateUserRes.status).toBe(200);
+    expect(updateUserRes.body.user.id).not.toBe(testUserId);
+    expect(updateUserRes.body.user.name).toBe("newTestUserName");
 })
 
 
