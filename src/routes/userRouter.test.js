b84f2a9e6593c3b655e2e348dccccc30c1a1e5b4
Jonah Larsen <Jonah.Larsen@gmail.com>
2026-02-19 19:31:02 -0700

    instruction setup of listusers

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -43,6 +43,31 @@ test('updateUser', async () => {
     expect(updateUserRes.status).toBe(200);
     expect(updateUserRes.body.user.id).toBe(testUserId);
     expect(updateUserRes.body.user.name).toBe(newUsername);
-})
+});
+
+test('list users unauthorized', async () => {
+  const listUsersRes = await request(app).get('/api/user');
+  expect(listUsersRes.status).toBe(401);
+});
+
+test('list users', async () => {
+  const [user, userToken] = await registerUser(request(app));
+  const listUsersRes = await request(app)
+    .get('/api/user')
+    .set('Authorization', 'Bearer ' + userToken);
+  expect(listUsersRes.status).toBe(200);
+});
+
+async function registerUser(service) {
+  const testUser = {
+    name: 'pizza diner',
+    email: `${randomName()}@test.com`,
+    password: 'a',
+  };
+  const registerRes = await service.post('/api/auth').send(testUser);
+  registerRes.body.user.password = testUser.password;
+
+  return [registerRes.body.user, registerRes.body.token];
+}
 
 
