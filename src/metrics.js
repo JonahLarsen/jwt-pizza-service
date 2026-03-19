8778345607bb4c117a31a802437d2c8f212b5e9a
Jonah Larsen <Jonah.Larsen@gmail.com>
2026-03-18 22:37:58 -0600

    basic parameterized metrics

--- src/metrics.js
+++ src/metrics.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const config = require('./config.js')
 
 function getCpuUsagePercentage() {
     const cpuUsage = os.loadavg()[0] / os.cpus().length;
@@ -11,4 +12,86 @@ function getMemoryUsagePercentage() {
     const usedMemory = totalMemory - freeMemory;
     const memoryUsage = (usedMemory / totalMemory) * 100;
     return memoryUsage.toFixed(2);
+}
+
+
+const requests = {};
+
+function requestTracker(req, res, next) {
+    const endpoint = `[${req.method}] ${req.path}`;
+    requests[endpoint] = (requests[endpoint] || 0) + 1;
+    next();
+}
+
+setInterval(() => {
+    const metrics = []
+    Object.keys(requests).forEach((endpoint) => {
+        metrics.push(createMetric('requests', requests[endpoint], '1', 'sum', 'asInt', { endpoint }));
+    });
+
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+    attributes = { ...attributes, source: config.source};
+
+    const metric = {
+        name: metricName,
+        unit: metricUnit,
+        [metricType]: {
+            dataPoints: [
+                {
+                    [valueType]: metricValue,
+                    timeUnixNano: Date.now() * 100000,
+                    attributes: [],
+                },
+            ],
+        },
+    };
+
+    Object.keys(attributes).forEach((key) => {
+        metric[metricType].dataPoints[0].attributes.push({
+            key: key,
+            value: { stringValue: attributes[key] },
+        });
+    });
+
+    if (metricType === 'sum') {
+        metric[metricType].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+        metric[metricType].isMonotonix = true;
+    }
+
+    return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+    const body = {
+        resourceMetrics: [
+            {
+                scopeMetrics: [
+                    {
+                        metrics,
+                    },
+                ],
+            },
+        ],
+    };
+
+    fetch(`${config.endpointUrl}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { Authorization: `Bearer ${config.accountId}:${config.apiKey}`, 'Content-Type': 'application/json' },
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP status: ${response.status}`);
+            }
+        })
+        .catch((error) => {
+            console.error('Error pushing metrics:', error);
+        });
+}
+
+module.exports = {
+    getCpuUsagePercentage,
+    getMemoryUsagePercentage
 }
